8fac5dac05f54ccfe152e8c631cb6d7329115cb9
thienlxp <thien.le@leap.expert>
2023-05-26 15:57:19 +0700

    ws: reconnect

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const WebSocket = require("ws");
-const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true, eot: true });
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -8,9 +8,9 @@ var cookie = process.env.COOKIE;
 
 var url = `wss://${process.env.ARGOCD_DOMAIN}/terminal?pod=${process.env.POD}&container=${process.env.CONTAINER}&appName=${process.env.APP_NAME}&appNamespace=${process.env.APP_NAMESPACE}&projectName=${process.env.PROJECT_NAME}&namespace=${process.env.NAMESPACE}`;
 
-const ws = new WebSocket(url, { headers: { Cookie: cookie } });
-
 var SHELL_PROMPTS = process.env.SHELL_PROMPTS.split(",");
+var reconnectInterval = 1_000;
+var ws;
 
 var isPrompt = (data) => {
   var prompts = SHELL_PROMPTS;
@@ -24,20 +24,39 @@ var isPrompt = (data) => {
   return false;
 };
 
-ws.on("open", function open() {
-  console.log("open ws connection...");
-});
+var connect = function () {
+  ws = new WebSocket(url, { headers: { Cookie: cookie } });
 
-ws.on("message", function message(data) {
-  // console.log('raw', data.toString());
-  var evt = JSON.parse(data.toString());
-  if (evt.operation == "stdout") {
-    if (isPrompt(evt.data)) {
-      var cmd = prompt(`${evt.data}`);
-      var sending = { operation: "stdin", data: cmd + "\r", cols: 0, rows: 0 };
-      ws.send(JSON.stringify(sending));
-    } else {
-      console.log(evt.data);
+  ws.on("open", function () {
+    console.log("open ws...");
+  });
+
+  ws.on("error", function () {
+    console.log("error ws");
+  });
+
+  ws.on("close", function () {
+    console.log("re-connect ws...");
+    setTimeout(connect, reconnectInterval);
+  });
+
+  ws.on("message", function message(data) {
+    var evt = JSON.parse(data.toString());
+    if (evt.operation == "stdout") {
+      if (isPrompt(evt.data)) {
+        var cmd = prompt(`${evt.data}`);
+        var sending = {
+          operation: "stdin",
+          data: cmd + "\r",
+          cols: 0,
+          rows: 0,
+        };
+        ws.send(JSON.stringify(sending));
+      } else {
+        process.stdout.write(evt.data);
+      }
     }
-  }
-});
+  });
+};
+
+connect();
